0911d54c0287fd523cfeea654b096cb069c200ca
Wasuphat Pudjaika <narayutput@gmail.com>
2022-08-07 22:31:24 +0700

    update: disable swagger doc in production

--- ansible/push-image.sh
+++ ansible/push-image.sh
@@ -1,13 +1,13 @@
 #!/bin/bash
 
-docker build -t ai4food-service:latest -t ai4food-service:$1 ..
+# docker build -t ai4food-service:latest -t ai4food-service:$1 ..
 
-docker tag ai4food-service:latest 430558697501.dkr.ecr.ap-southeast-1.amazonaws.com/ai4food-service:latest
+# docker tag ai4food-service:latest 430558697501.dkr.ecr.ap-southeast-1.amazonaws.com/ai4food-service:latest
 
-docker tag ai4food-service:$1 430558697501.dkr.ecr.ap-southeast-1.amazonaws.com/ai4food-service:$1
+# docker tag ai4food-service:$1 430558697501.dkr.ecr.ap-southeast-1.amazonaws.com/ai4food-service:$1
 
-docker push 430558697501.dkr.ecr.ap-southeast-1.amazonaws.com/ai4food-service:latest
+# docker push 430558697501.dkr.ecr.ap-southeast-1.amazonaws.com/ai4food-service:latest
 
-docker push 430558697501.dkr.ecr.ap-southeast-1.amazonaws.com/ai4food-service:$1
+# docker push 430558697501.dkr.ecr.ap-southeast-1.amazonaws.com/ai4food-service:$1
 
-docker-compose run --env IMAGE_TAG=$1 --rm ansible
+docker-compose run -e IMAGE_TAG=$1 --rm ansible
