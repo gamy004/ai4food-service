22ce8fac7b21f915ead77394dc477730cfb43293
Wasuphat Pudjaika <narayutput@gmail.com>
2022-09-06 00:19:27 +0700

    update: update api to response data as separated entities

---
+++ src/swab/dto/response-query-lab-swab-plan.dto.ts
@@ -0,0 +1,20 @@
+import { FacilityItem } from '~/facility/entities/facility-item.entity';
+import { Facility } from '~/facility/entities/facility.entity';
+import { SwabAreaHistory } from '../entities/swab-area-history.entity';
+import { SwabArea } from '../entities/swab-area.entity';
+import { SwabPeriod } from '../entities/swab-period.entity';
+import { SwabTest } from '../entities/swab-test.entity';
+
+export class ResponseQueryLabSwabPlanDto {
+  swabAreaHistories: SwabAreaHistory[];
+
+  swabTests: SwabTest[];
+
+  swabAreas: SwabArea[];
+
+  swabPeriods: SwabPeriod[];
+
+  facilities: Facility[];
+
+  facilityitems: FacilityItem[];
+}
