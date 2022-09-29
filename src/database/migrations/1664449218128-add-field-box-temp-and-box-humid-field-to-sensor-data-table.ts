1ca880d1dd903f8f6357d98768ec7bb953edd2e3
Wasuphat Pudjaika <narayutput@gmail.com>
2022-09-30 00:05:25 +0700

    update: data fields on product and sensor data table

---
+++ src/database/migrations/1664449218128-add-field-box-temp-and-box-humid-field-to-sensor-data-table.ts
@@ -0,0 +1,16 @@
+import { MigrationInterface, QueryRunner } from "typeorm";
+
+export class addFieldBoxTempAndBoxHumidFieldToSensorDataTable1664449218128 implements MigrationInterface {
+    name = 'addFieldBoxTempAndBoxHumidFieldToSensorDataTable1664449218128'
+
+    public async up(queryRunner: QueryRunner): Promise<void> {
+        await queryRunner.query(`ALTER TABLE \`sensor_data\` ADD \`box_temperature\` float NULL`);
+        await queryRunner.query(`ALTER TABLE \`sensor_data\` ADD \`box_humidity\` float NULL`);
+    }
+
+    public async down(queryRunner: QueryRunner): Promise<void> {
+        await queryRunner.query(`ALTER TABLE \`sensor_data\` DROP COLUMN \`box_humidity\``);
+        await queryRunner.query(`ALTER TABLE \`sensor_data\` DROP COLUMN \`box_temperature\``);
+    }
+
+}
