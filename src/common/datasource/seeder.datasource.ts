df820caf49e4021af15b88cb46477bfde73ad015
Wasuphat Pudjaika <narayutput@gmail.com>
2022-11-16 01:30:17 +0700

    update: - change dto where deletedAt value from null to isNull() - update seeder seeds

--- src/common/datasource/seeder.datasource.ts
+++ src/common/datasource/seeder.datasource.ts
@@ -1,30 +1,32 @@
 import { DataSource, DataSourceOptions } from 'typeorm';
 import { runSeeders, SeederOptions } from 'typeorm-extension';
-import typeormConfig from "../../database/config/typeorm.config";
+import typeormConfig from '../../database/config/typeorm.config';
 
 (async () => {
-    const options: DataSourceOptions & SeederOptions = {
-        ...typeormConfig,
-        migrationsRun: false,
-        seeds: [
-            "dist/database/seeders/bacteria.seeder.js",
-            "dist/database/seeders/swab-environment.seeder.js",
-        ]
-    };
+  const options: DataSourceOptions & SeederOptions = {
+    ...typeormConfig,
+    migrationsRun: false,
+    entities: ['dist/**/*.entity.js'],
+    seeds: [
+      'dist/database/seeders/user.seeder.js',
+      'dist/database/seeders/bacteria.seeder.js',
+      'dist/database/seeders/swab-environment.seeder.js',
+    ],
+  };
 
-    console.log("===========Start Seeding!!!===========");
+  console.log('===========Start Seeding!!!===========');
 
-    const dataSource = new DataSource(options);
+  const dataSource = new DataSource(options);
 
-    await dataSource.initialize();
+  await dataSource.initialize();
 
-    await dataSource.runMigrations();
+  await dataSource.runMigrations();
 
-    await runSeeders(dataSource);
+  await runSeeders(dataSource);
 
-    await dataSource.destroy();
+  await dataSource.destroy();
 
-    console.log("===========Finish Seeding!!!===========");
+  console.log('===========Finish Seeding!!!===========');
 
-    process.exit(1);
+  process.exit(1);
 })();
