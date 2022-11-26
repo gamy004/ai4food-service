4a60e7c4e155ad81a3dba2076556042e5451ea72
Wasuphat Pudjaika <narayutput@gmail.com>
2022-11-26 17:47:21 +0700

    feat(AI4F-126): add querying swab product histoy with export API

--- src/swab/dto/response-swab-plan.dto.ts
+++ src/swab/dto/response-swab-plan.dto.ts
@@ -1,14 +1,20 @@
 import { Facility } from "~/facility/entities/facility.entity";
+import { Product } from "~/product/entities/product.entity";
 import { SwabAreaHistory } from "../entities/swab-area-history.entity";
 import { SwabArea } from "../entities/swab-area.entity";
 import { SwabPeriod } from "../entities/swab-period.entity";
+import { SwabProductHistory } from "../entities/swab-product-history.entity";
 
 export class ResponseSwabPlanDto {
     swabPeriods: SwabPeriod[];
 
     swabAreaHistories: SwabAreaHistory[];
 
+    swabProductHistories: SwabProductHistory[];
+
     swabAreas: SwabArea[];
+
+    products: Product[];
 
     facilities: Facility[];
 }
