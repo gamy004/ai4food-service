c49a64f7ad0b9835819b385f251c4d9d0c86f551
Wasuphat Pudjaika <narayutput@gmail.com>
2022-11-29 23:28:16 +0700

    feat(AI4F-128): add sensor mapping to map node_no with facility item id

--- src/facility/entities/facility-item.entity.ts
+++ src/facility/entities/facility-item.entity.ts
@@ -1,38 +1,49 @@
-import { Column, Entity, ManyToOne, OneToMany } from "typeorm";
-import { BaseSoftDeletableEntity } from "~/common/entities/base-softdeletable.entity";
-import { SwabAreaHistory } from "~/swab/entities/swab-area-history.entity";
-import { SwabArea } from "~/swab/entities/swab-area.entity";
-import { SwabProductHistory } from "~/swab/entities/swab-product-history.entity";
-import { Facility } from "./facility.entity";
-import { Room } from "./room.entity";
-import { Zone } from "./zone.entity";
+import { Column, Entity, ManyToOne, OneToMany } from 'typeorm';
+import { BaseSoftDeletableEntity } from '~/common/entities/base-softdeletable.entity';
+import { SensorMapping } from '~/iot/entities/sensor-mapping.entity';
+import { SwabAreaHistory } from '~/swab/entities/swab-area-history.entity';
+import { SwabProductHistory } from '~/swab/entities/swab-product-history.entity';
+import { Facility } from './facility.entity';
+import { Room } from './room.entity';
+import { Zone } from './zone.entity';
 
 @Entity()
 export class FacilityItem extends BaseSoftDeletableEntity {
-    @Column({ unique: true })
-    facilityItemName!: string;
+  @Column({ unique: true })
+  facilityItemName!: string;
 
-    @Column({ type: "varchar", length: 36 })
-    facilityId!: string;
+  @Column({ type: 'varchar', length: 36 })
+  facilityId!: string;
 
-    @ManyToOne(() => Facility, entity => entity.facilityItems, { onDelete: 'CASCADE' })
-    facility!: Facility;
+  @ManyToOne(() => Facility, (entity) => entity.facilityItems, {
+    onDelete: 'CASCADE',
+  })
+  facility!: Facility;
 
-    @Column({ type: "varchar", length: 36, nullable: true })
-    roomId?: string;
+  @Column({ type: 'varchar', length: 36, nullable: true })
+  roomId?: string;
 
-    @ManyToOne(() => Room, entity => entity.facilityItems, { nullable: true, onDelete: 'SET NULL' })
-    room?: Room;
+  @ManyToOne(() => Room, (entity) => entity.facilityItems, {
+    nullable: true,
+    onDelete: 'SET NULL',
+  })
+  room?: Room;
 
-    @Column({ type: "varchar", length: 36, nullable: true })
-    zoneId?: string;
+  @Column({ type: 'varchar', length: 36, nullable: true })
+  zoneId?: string;
 
-    @ManyToOne(() => Zone, entity => entity.facilityItems, { nullable: true, onDelete: 'SET NULL' })
-    zone?: Zone;
+  @ManyToOne(() => Zone, (entity) => entity.facilityItems, {
+    nullable: true,
+    onDelete: 'SET NULL',
+  })
+  zone?: Zone;
 
-    @OneToMany(() => SwabAreaHistory, entity => entity.facilityItem)
-    swabAreaHistories: SwabAreaHistory[];
+  @OneToMany(() => SwabAreaHistory, (entity) => entity.facilityItem)
+  swabAreaHistories: SwabAreaHistory[];
 
-    @OneToMany(() => SwabProductHistory, entity => entity.facilityItem)
-    swabProductHistories: SwabProductHistory[];
+  @OneToMany(() => SwabProductHistory, (entity) => entity.facilityItem)
+  swabProductHistories: SwabProductHistory[];
+
+  @OneToMany(() => SensorMapping, (entity) => entity.facilityItem)
+  sensorMappings: SensorMapping[];
 }
--- src/swab/entities/swab-period.entity.ts
+++ src/swab/entities/swab-period.entity.ts
@@ -1,17 +1,16 @@
-
-import { Column, Entity, JoinColumn, ManyToOne, OneToMany, OneToOne } from "typeorm";
-import { BaseSoftDeletableEntity } from "~/common/entities/base-softdeletable.entity";
-import { SwabAreaHistory } from "./swab-area-history.entity";
-import { SwabProductHistory } from "./swab-product-history.entity";
+import { Column, Entity, OneToMany } from 'typeorm';
+import { BaseSoftDeletableEntity } from '~/common/entities/base-softdeletable.entity';
+import { SwabAreaHistory } from './swab-area-history.entity';
+import { SwabProductHistory } from './swab-product-history.entity';
 
 @Entity()
 export class SwabPeriod extends BaseSoftDeletableEntity {
-    @Column({ unique: true })
-    swabPeriodName!: string;
+  @Column({ unique: true })
+  swabPeriodName!: string;
 
-    @OneToMany(() => SwabAreaHistory, entity => entity.swabPeriod)
-    swabAreaHistories: SwabAreaHistory[];
+  @OneToMany(() => SwabAreaHistory, (entity) => entity.swabPeriod)
+  swabAreaHistories: SwabAreaHistory[];
 
-    @OneToMany(() => SwabProductHistory, entity => entity.swabPeriod)
-    swabProductHistories: SwabProductHistory[];
+  @OneToMany(() => SwabProductHistory, (entity) => entity.swabPeriod)
+  swabProductHistories: SwabProductHistory[];
 }
