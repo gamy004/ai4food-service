0f7ac65001f94209089c9fc74160708465779cd0
Wasuphat Pudjaika <narayutput@gmail.com>
2022-12-17 14:35:22 +0700

    fix(AI4F-132): change swabAreaSwabedAt and facilityItem field to optional fields

--- src/swab/dto/command-update-swab-plan-by-id.dto.ts
+++ src/swab/dto/command-update-swab-plan-by-id.dto.ts
@@ -1,63 +1,72 @@
-import { Type } from "class-transformer";
-import { IsNotEmpty, IsOptional, IsUUID, Max, Min, Validate, ValidateNested } from "class-validator";
-import { DateOnlyRule } from "~/common/validators/date-only-validator";
-import { TimeOnlyRule } from "~/common/validators/time-only-validator";
-import { ConnectFacilityItemDto } from "~/facility/dto/connect-facility-item.dto";
-import { ConnectProductDto } from "~/product/dto/connect-product.dto";
-import { SwabAreaHistoryExistsRule } from "../validators/swab-area-history-exists-validator";
-import { UpsertSwabAreaHistoryImageDto } from "./upsert-swab-area-history-image.dto";
-import { UpsertSwabEnvironmentDto } from "./upsert-swab-environment.dto";
+import { Type } from 'class-transformer';
+import {
+  IsNotEmpty,
+  IsOptional,
+  IsUUID,
+  Max,
+  Min,
+  Validate,
+  ValidateNested,
+} from 'class-validator';
+import { DateOnlyRule } from '~/common/validators/date-only-validator';
+import { TimeOnlyRule } from '~/common/validators/time-only-validator';
+import { ConnectFacilityItemDto } from '~/facility/dto/connect-facility-item.dto';
+import { ConnectProductDto } from '~/product/dto/connect-product.dto';
+import { SwabAreaHistoryExistsRule } from '../validators/swab-area-history-exists-validator';
+import { UpsertSwabAreaHistoryImageDto } from './upsert-swab-area-history-image.dto';
+import { UpsertSwabEnvironmentDto } from './upsert-swab-environment.dto';
 
 export class ParamCommandUpdateSwabPlanByIdDto {
-    @IsUUID()
-    @Validate(SwabAreaHistoryExistsRule)
-    id: string;
-
+  @IsUUID()
+  @Validate(SwabAreaHistoryExistsRule)
+  id: string;
 }
 
 export class BodyCommandUpdateSwabPlanByIdDto {
-    @IsNotEmpty()
-    @Validate(TimeOnlyRule)
-    swabAreaSwabedAt: string;
+  @IsOptional()
+  @IsNotEmpty()
+  @Validate(TimeOnlyRule)
+  swabAreaSwabedAt: string;
 
-    @ValidateNested()
-    @Type(() => ConnectFacilityItemDto)
-    facilityItem!: ConnectFacilityItemDto;
+  @IsOptional()
+  @ValidateNested()
+  @Type(() => ConnectFacilityItemDto)
+  facilityItem!: ConnectFacilityItemDto;
 
-    @IsOptional()
-    @IsNotEmpty()
-    @Min(0)
-    @Max(100)
-    swabAreaTemperature?: number;
+  @IsOptional()
+  @IsNotEmpty()
+  @Min(0)
+  @Max(100)
+  swabAreaTemperature?: number;
 
-    @IsOptional()
-    @IsNotEmpty()
-    @Min(0)
-    @Max(100)
-    swabAreaHumidity?: number;
+  @IsOptional()
+  @IsNotEmpty()
+  @Min(0)
+  @Max(100)
+  swabAreaHumidity?: number;
 
-    @IsOptional()
-    @IsNotEmpty()
-    swabAreaNote?: string;
+  @IsOptional()
+  @IsNotEmpty()
+  swabAreaNote?: string;
 
-    @IsOptional()
-    @ValidateNested()
-    @Type(() => ConnectProductDto)
-    product?: ConnectProductDto;
+  @IsOptional()
+  @ValidateNested()
+  @Type(() => ConnectProductDto)
+  product?: ConnectProductDto;
 
-    @IsOptional()
-    @Validate(DateOnlyRule)
-    productDate?: string;
+  @IsOptional()
+  @Validate(DateOnlyRule)
+  productDate?: string;
 
-    @IsOptional()
-    @IsNotEmpty()
-    productLot?: string;
+  @IsOptional()
+  @IsNotEmpty()
+  productLot?: string;
 
-    @ValidateNested({ each: true })
-    @Type(() => UpsertSwabEnvironmentDto)
-    swabEnvironments: UpsertSwabEnvironmentDto[];
+  @ValidateNested({ each: true })
+  @Type(() => UpsertSwabEnvironmentDto)
+  swabEnvironments: UpsertSwabEnvironmentDto[];
 
-    @ValidateNested({ each: true })
-    @Type(() => UpsertSwabAreaHistoryImageDto)
-    swabAreaHistoryImages: UpsertSwabAreaHistoryImageDto[];
+  @ValidateNested({ each: true })
+  @Type(() => UpsertSwabAreaHistoryImageDto)
+  swabAreaHistoryImages: UpsertSwabAreaHistoryImageDto[];
 }
