514a5beaa29ab0fe96a7153e916a30e265b80adf
chanuwat_may <porclub.za@gmail.com>
2022-12-19 15:38:47 +0700

    update: seeder file

--- src/common/datasource/seeder.datasource.ts
+++ src/common/datasource/seeder.datasource.ts
@@ -8,10 +8,15 @@ import typeormConfig from '../../database/config/typeorm.config';
     migrationsRun: false,
     entities: ['dist/**/*.entity.js'],
     seeds: [
+      'dist/database/seeders/bacteria.seeder.js',
       'dist/database/seeders/contact-zone.seeder.js',
-      'dist/database/seeders/swab.seeder.js',
       'dist/database/seeders/risk-zone.seeder.js',
+      'dist/database/seeders/product.seeder.js',
+      'dist/database/seeders/swab-environment.seeder.js',
+      'dist/database/seeders/swab-period.seeder.js',
       'dist/database/seeders/facility.seeder.js',
+      'dist/database/seeders/swab.seeder.js',
+      'dist/database/seeders/user.seeder.js',
     ],
   };
 
