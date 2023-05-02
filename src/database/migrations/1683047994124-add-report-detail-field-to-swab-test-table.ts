66fa692a339d369d985109c1dba15ea9a401e497
Wasuphat Pudjaika <narayutput@gmail.com>
2023-05-03 00:21:20 +0700

    feat(AI4F-158): add field `report_detail`

---
+++ src/database/migrations/1683047994124-add-report-detail-field-to-swab-test-table.ts
@@ -0,0 +1,14 @@
+import { MigrationInterface, QueryRunner } from "typeorm";
+
+export class addReportDetailFieldToSwabTestTable1683047994124 implements MigrationInterface {
+    name = 'addReportDetailFieldToSwabTestTable1683047994124'
+
+    public async up(queryRunner: QueryRunner): Promise<void> {
+        await queryRunner.query(`ALTER TABLE \`swab_test\` ADD \`report_detail\` text NULL`);
+    }
+
+    public async down(queryRunner: QueryRunner): Promise<void> {
+        await queryRunner.query(`ALTER TABLE \`swab_test\` DROP COLUMN \`report_detail\``);
+    }
+
+}
